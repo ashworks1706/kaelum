1e2d9aff31a49eb2667597620b8a220977cf8a43
ashworks1706 <asriv132@asu.edu>
2025-11-02 00:44:20 -0700

    Added streaming

--- kaelum/plugins/__init__.py
+++ kaelum/plugins/__init__.py
@@ -2,5 +2,14 @@
 
 from kaelum.plugins.base import KaelumPlugin
 from kaelum.plugins.reasoning import ReasoningPlugin
+from kaelum.plugins.planning import PlanningPlugin
+from kaelum.plugins.routing import RoutingPlugin
+from kaelum.plugins.vision import VisionPlugin
 
-__all__ = ["KaelumPlugin", "ReasoningPlugin"]
+__all__ = [
+    "KaelumPlugin",
+    "ReasoningPlugin",
+    "PlanningPlugin",
+    "RoutingPlugin",
+    "VisionPlugin",
+]
