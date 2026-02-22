0b4fcba0b4e79be270c8df9e048d006ed7caf8af
ashworks1706 <asriv132@asu.edu>
2026-02-21 17:42:57 -0700

    replacing heuristics

--- core/verification/__init__.py
+++ core/verification/__init__.py
@@ -3,9 +3,6 @@
 from .verification import VerificationEngine, SymbolicVerifier
 from .reflection import ReflectionEngine
 from .sympy_engine import SympyEngine
-from .relevance_validator import RelevanceValidator
-from .confidence_calibrator import ConfidenceCalibrator
-from .threshold_calibrator import ThresholdCalibrator
 from .process_reward_model import ProcessRewardModel, get_prm
 
 __all__ = [
@@ -13,9 +10,6 @@ __all__ = [
     'SymbolicVerifier',
     'ReflectionEngine',
     'SympyEngine',
-    'RelevanceValidator',
-    'ConfidenceCalibrator',
-    'ThresholdCalibrator',
     'ProcessRewardModel',
     'get_prm',
 ]
